29b8279dcacfa5b28049046abbdc92755b834f95
Cassunshine <cass.stark.sunshine@gmail.com>
2025-07-24 14:57:53 -0400

    Update README and change PascalCase to camelCase for the functions :)

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # fn2zig
-FastNoise2 bindings for Zig
+FastNoise2 binding/wrapper for Zig.
 
-Made in a day or two, entirely incomplete, but slightly ziggified.
+Made in a day or two, super incomplete, but slightly ziggified.
 
+To include in your project, run zig fetch like normal, then add this to your build.zig:
+```zig
+    const fn2zig = b.dependency("fn2zig", .{});
+    module.addImport("fn2zig", fn2zig.module("fn2zig"));
+```
+
+Example use:
+```zig
+    //Import, obviously
+    const fn2zig = @import("fn2zig");
+
+    // Create a node from encoded tree
+    const node = fn2zig.fromEncodedTree("DQAFAAAAAAAAQAgAAAAAAD8AAAAAAA==");
+
+    // genUniform, genPositionArray, and genSingle are available in 2d, 3d, and 4d.
+    node.genSingle2D(x, y, seed);
+```
+
+
+## Notes
+
+This isn't a proper full wrapper, we don't have all the FastNoise2 functions and types ziggified yet. If you wanna use the C API directly, `fn2zig.FastNoiseC` has all the functions exposed. You can just pass in the value of the `ptr` field from a SmartNode and it should work.
+
+This library also uses Zig's build system to compile FastNoise2, rather than using prebuilt binaries or requiring the user to install CMake. The early stages of this project mean that none of the options you can find for FastNoise2's CMake project exist yet, and I doubt ARM will compile correctly. I wanna make sure that stuff works later, though.
+
+This library is purely for the FastNoise2 library itself, it doesn't compile or touch NoiseTool in any way.
--- src/fn2zig.zig
+++ src/fn2zig.zig
@@ -1,14 +1,12 @@
 const std = @import("std");
 const testing = std.testing;
 
-const FastNoiseC = @cImport({
+pub const FastNoiseC = @cImport({
     @cInclude("stdbool.h");
     @cInclude("FastNoise/FastNoise_C.h");
 });
 
-pub const Generators = @import("generators/Generators.zig");
-
-pub fn FromEncodedNodeTree(encoded: []const u8) !SmartNode {
+pub fn fromEncodedTree(encoded: []const u8) !SmartNode {
     const ptr = FastNoiseC.fnNewFromEncodedNodeTree(encoded, 0);
 
     if (ptr == null) {
@@ -21,63 +19,63 @@ pub fn FromEncodedNodeTree(encoded: []const u8) !SmartNode {
 pub const SmartNode = struct {
     ptr: *anyopaque,
 
-    pub fn GenUniformGrid2D(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, xSize: u32, ySize: u32, frequency: f32, seed: u32) void {
+    pub fn genUniformGrid2D(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, xSize: u32, ySize: u32, frequency: f32, seed: u32) void {
         FastNoiseC.fnGenUniformGrid2D(self.ptr, target.ptr, xStart, yStart, xSize, ySize, frequency, seed, &.{ 0, 0 });
     }
 
-    pub fn GenUniformGrid3D(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, xSize: u32, ySize: u32, zSize: u32, frequency: f32, seed: u32) void {
+    pub fn genUniformGrid3D(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, xSize: u32, ySize: u32, zSize: u32, frequency: f32, seed: u32) void {
         FastNoiseC.fnGenUniformGrid3D(self.ptr, target.ptr, xStart, yStart, zStart, xSize, ySize, zSize, frequency, seed, &.{ 0, 0 });
     }
 
-    pub fn GenUniformGrid4D(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, wStart: i32, xSize: u32, ySize: u32, zSize: u32, wSize: u32, frequency: f32, seed: u32) void {
+    pub fn genUniformGrid4D(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, wStart: i32, xSize: u32, ySize: u32, zSize: u32, wSize: u32, frequency: f32, seed: u32) void {
         FastNoiseC.fnGenUniformGrid4D(self.ptr, target.ptr, xStart, yStart, zStart, wStart, xSize, ySize, zSize, wSize, frequency, seed, &.{ 0, 0 });
     }
 
-    pub fn GenUniformGrid2DOuputMinMax(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, xSize: u32, ySize: u32, frequency: f32, seed: u32, outputMinMax: [2]f32) void {
+    pub fn genUniformGrid2DOuputMinMax(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, xSize: u32, ySize: u32, frequency: f32, seed: u32, outputMinMax: [2]f32) void {
         FastNoiseC.fnGenUniformGrid2D(self.ptr, target.ptr, xStart, yStart, xSize, ySize, frequency, seed, outputMinMax);
     }
 
-    pub fn GenUniformGrid3DOuputMinMax(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, xSize: u32, ySize: u32, zSize: u32, frequency: f32, seed: u32, outputMinMax: [2]f32) void {
+    pub fn genUniformGrid3DOuputMinMax(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, xSize: u32, ySize: u32, zSize: u32, frequency: f32, seed: u32, outputMinMax: [2]f32) void {
         FastNoiseC.fnGenUniformGrid3D(self.ptr, target.ptr, xStart, yStart, zStart, xSize, ySize, zSize, frequency, seed, outputMinMax);
     }
 
-    pub fn GenUniformGrid4DOuputMinMax(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, wStart: i32, xSize: u32, ySize: u32, zSize: u32, wSize: u32, frequency: f32, seed: u32, outputMinMax: [2]f32) void {
+    pub fn genUniformGrid4DOuputMinMax(self: *SmartNode, target: []f32, xStart: i32, yStart: i32, zStart: i32, wStart: i32, xSize: u32, ySize: u32, zSize: u32, wSize: u32, frequency: f32, seed: u32, outputMinMax: [2]f32) void {
         FastNoiseC.fnGenUniformGrid4D(self.ptr, target.ptr, xStart, yStart, zStart, wStart, xSize, ySize, zSize, wSize, frequency, seed, outputMinMax);
     }
 
-    pub fn GenPositionArray2D(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, xOffset: f32, yOffset: f32, seed: u32) void {
+    pub fn genPositionArray2D(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, xOffset: f32, yOffset: f32, seed: u32) void {
         FastNoiseC.fnGenPositionArray2D(self.ptr, target.ptr, xPositions.len, xPositions.ptr, yPositions.ptr, xOffset, yOffset, seed, &.{ 0, 0 });
     }
 
-    pub fn GenPositionArray3D(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, seed: u32) void {
+    pub fn genPositionArray3D(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, seed: u32) void {
         FastNoiseC.fnGenPositionArray3D(self.ptr, target.ptr, xPositions.len, xPositions.ptr, yPositions.ptr, zPositions.ptr, xOffset, yOffset, zOffset, seed, &.{ 0, 0 });
     }
 
-    pub fn GenPositionArray4D(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, wPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, wOffset: f32, seed: u32) void {
+    pub fn genPositionArray4D(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, wPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, wOffset: f32, seed: u32) void {
         FastNoiseC.fnGenPositionArray4D(self.ptr, target.ptr, xPositions.len, xPositions.ptr, yPositions.ptr, zPositions.ptr, wPositions.ptr, xOffset, yOffset, zOffset, wOffset, seed, &.{ 0, 0 });
     }
 
-    pub fn GenPositionArray2DOuputMinMax(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, xOffset: f32, yOffset: f32, seed: u32, outputMinMax: [2]f32) void {
+    pub fn genPositionArray2DOuputMinMax(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, xOffset: f32, yOffset: f32, seed: u32, outputMinMax: [2]f32) void {
         FastNoiseC.fnGenPositionArray2D(self.ptr, target.ptr, xPositions.len, xPositions.ptr, yPositions.ptr, xOffset, yOffset, seed, outputMinMax);
     }
 
-    pub fn GenPositionArray3DOuputMinMax(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, seed: u32, outputMinMax: [2]f32) void {
+    pub fn genPositionArray3DOuputMinMax(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, seed: u32, outputMinMax: [2]f32) void {
         FastNoiseC.fnGenPositionArray3D(self.ptr, target.ptr, xPositions.len, xPositions.ptr, yPositions.ptr, zPositions.ptr, xOffset, yOffset, zOffset, seed, outputMinMax);
     }
 
-    pub fn GenPositionArray4DOuputMinMax(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, wPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, wOffset: f32, seed: u32, outputMinMax: [2]f32) void {
+    pub fn genPositionArray4DOuputMinMax(self: *SmartNode, target: []f32, xPositions: []f32, yPositions: []f32, zPositions: []f32, wPositions: []f32, xOffset: f32, yOffset: f32, zOffset: f32, wOffset: f32, seed: u32, outputMinMax: [2]f32) void {
         FastNoiseC.fnGenPositionArray4D(self.ptr, target.ptr, xPositions.len, xPositions.ptr, yPositions.ptr, zPositions.ptr, wPositions.ptr, xOffset, yOffset, zOffset, wOffset, seed, outputMinMax);
     }
 
-    pub fn GenSingle2D(self: *SmartNode, x: f32, y: f32, seed: u32) f32 {
+    pub fn genSingle2D(self: *SmartNode, x: f32, y: f32, seed: u32) f32 {
         return FastNoiseC.fnGenSingle2D(self.ptr, x, y, seed);
     }
 
-    pub fn GenSingle3D(self: *SmartNode, x: f32, y: f32, z: f32, seed: u32) f32 {
+    pub fn genSingle3D(self: *SmartNode, x: f32, y: f32, z: f32, seed: u32) f32 {
         return FastNoiseC.fnGenSingle3D(self.ptr, x, y, z, seed);
     }
 
-    pub fn GenSingle4D(self: *SmartNode, x: f32, y: f32, z: f32, w: f32, seed: u32) f32 {
+    pub fn genSingle4D(self: *SmartNode, x: f32, y: f32, z: f32, w: f32, seed: u32) f32 {
         return FastNoiseC.fnGenSingle4D(self.ptr, x, y, z, w, seed);
     }
 };
